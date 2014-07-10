589af107d395838b4e7271043de22678e35a3db3
macheteshuffle <jeff.li029@gmail.com>
2014-07-09 21:21:27 -0400

    created rspec tests

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Account do
   it { should have_valid(:name).when('Jeff Li') }
+  it { should_not have_valid(:name).when('') }
 
   it { should have_many(:domains) }
 
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe Domain do
   it { should have_valid(:hostname).when('http://localhost:3000') }
   it { should have_valid(:account_id).when(1) }
+  it { should have_valid(:ip_address).when('127.0.0.1') }
+
+  it { should_not have_valid(:hostname).when('') }
+  it { should_not have_valid(:account_id).when('') }
+  it { should_not have_valid(:ip_address).when('') }
+
 
   it { should belong_to(:account) }
 
