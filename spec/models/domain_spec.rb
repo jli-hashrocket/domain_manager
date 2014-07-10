bc471eb684e780b7416b2f68689068c21e5018a6
macheteshuffle <jeff.li029@gmail.com>
2014-07-09 23:47:01 -0400

    added another test

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -9,8 +9,12 @@ describe Domain do
   it { should_not have_valid(:account_id).when('') }
   it { should_not have_valid(:ip_address).when('') }
 
-
   it { should belong_to(:account) }
 
-
+  context 'gets ip address' do
+    it 'should return an ip address when hostname is given' do
+      ip = Domain.get_ip('google.com')
+      expect(ip).to eql('74.125.228.103')
+    end
+  end
 end
